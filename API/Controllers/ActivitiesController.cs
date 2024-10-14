0293125cdfe69d4b36189a0604c628081b706421
Hadi-SayedAhmad <hadi.github.acc@gmail.com>
2024-10-14 17:38:50 +0300

    Walking skeleton - Activities controller and db conn setup

---
+++ API/Controllers/ActivitiesController.cs
@@ -0,0 +1,34 @@
+﻿using Domain;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Persistence;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ActivitiesController : ControllerBase
+    {
+        public ActivitiesController(DataContext context)
+        {
+            _context = context;
+        }
+
+        private readonly DataContext _context;
+
+        [HttpGet]
+        public async Task<ActionResult<List<Activity>>> GetActivities()
+        {
+            return await _context.Activities.ToListAsync();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Activity>> GetActivity(Guid id)
+        {
+            return await _context.Activities.FindAsync(id);
+        }
+    }
+
+    
+}
